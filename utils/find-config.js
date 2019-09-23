ed3bff42daf06b5877080b59494ebe6cb9d14f30
JR Shampang <jr@shampang.com>
2019-09-22 21:04:11 -0500

    ♻️ extract default config logic

--- utils/find-config.js
+++ utils/find-config.js
@@ -2,16 +2,18 @@
 
 const cosmiconfig = require('cosmiconfig')
 
+const getDefaultConfig = name => {
+  return {
+    config: require(`../config/${name}`),
+    filepath: require.resolve(`../config/${name}`),
+    isEmpty: false,
+  }
+}
+
 // Try to find the user's config.
 // If one isn't found, return a default config.
 module.exports = (name, options = {}) => {
   const explorer = cosmiconfig(name, options)
   const results = explorer.searchSync()
-  return (
-    results || {
-      config: require(`../config/${name}`),
-      filepath: require.resolve(`../config/${name}`),
-      isEmpty: false,
-    }
-  )
+  return results || getDefaultConfig(name)
 }
