53d8181d23d1c4a138a9ca368b40774026dfad5d
JR Shampang <jr@shampang.com>
2019-10-07 00:02:07 -0500

    ✨ add lib to jest coverage

--- config/jest.js
+++ config/jest.js
@@ -8,7 +8,7 @@ const rootDir = resolveProjectPath('.')
 module.exports = {
   clearMocks: true,
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{js,jsx}'],
+  collectCoverageFrom: ['src/**/*.{js,jsx}', 'lib/**/*.js'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': require.resolve(
       './file-stub.js'
