f3cbb77de4568a09e9fd684d50a9d02a57156722
JR Shampang <jr@shampang.com>
2019-10-07 00:06:58 -0500

    📝 add example projects

---
+++ examples/react-app/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+console.log('oh no')
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img alt='logo' className='App-logo' src={logo} />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className='App-link'
+          href='https://reactjs.org'
+          rel='noopener noreferrer'
+          target='_blank'
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App
