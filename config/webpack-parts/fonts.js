a84f2334497d13bb66487871731e0b71793bbfeb
JR Shampang <jr@shampang.com>
2019-12-18 23:49:53 -0600

    ✨ update font and image filenames

--- config/webpack-parts/fonts.js
+++ config/webpack-parts/fonts.js
@@ -3,7 +3,14 @@
 module.exports = config => {
   config.module.rules.push({
     test: /\.(woff|woff2)$/,
-    use: [require.resolve('file-loader')],
+    use: [
+      {
+        loader: require.resolve('file-loader'),
+        options: {
+          name: '[name].[contenthash:8].[ext]',
+        },
+      },
+    ],
   })
 
   return config
--- config/webpack-parts/images.js
+++ config/webpack-parts/images.js
@@ -3,7 +3,14 @@
 module.exports = config => {
   config.module.rules.push({
     test: /\.(gif|jpeg|jpg|png|svg)$/,
-    use: [require.resolve('file-loader')],
+    use: [
+      {
+        loader: require.resolve('file-loader'),
+        options: {
+          name: '[name].[contenthash:8].[ext]',
+        },
+      },
+    ],
   })
 
   return config
