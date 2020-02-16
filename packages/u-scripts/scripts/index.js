ef4d37baa77c19707db8d85a1129a3f912122eb0
Cary Wolff <carywolff@northwesternmutual.com>
2020-02-16 13:26:49 -0600

    ✨ add nodemon

--- packages/u-scripts/scripts/index.js
+++ packages/u-scripts/scripts/index.js
@@ -1,18 +1,19 @@
-'use strict'
+"use strict";
 
-const path = require('path')
+const path = require("path");
 
 const scripts = [
-  'babel',
-  'eslint',
-  'jest',
-  'prettier',
-  'stylelint',
-  'webpack',
-  'webpack-dev-server',
+  "babel",
+  "eslint",
+  "jest",
+  "nodemon",
+  "prettier",
+  "stylelint",
+  "webpack",
+  "webpack-dev-server"
 ].reduce((a, b) => {
-  a[b] = path.join(__dirname, b)
-  return a
-}, {})
+  a[b] = path.join(__dirname, b);
+  return a;
+}, {});
 
-module.exports = scripts
+module.exports = scripts;
