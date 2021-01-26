d5adc7ce1b5a04226c014ae28ac54ed231c3f494
JR Shampang <jr@shampang.com>
2021-01-26 08:36:03 -0600

    fix: 🎨 move no-undef override to eslint.js

--- packages/eslint-config/rules/eslint.js
+++ packages/eslint-config/rules/eslint.js
@@ -3,6 +3,17 @@
 // 📐 eslint: https://eslint.org/docs/rules/
 module.exports = {
   extends: ['eslint:recommended'],
+  overrides: [
+    {
+      files: ['*.ts?(x)'],
+      rules: {
+        // ESLint is producing false positives for this rule in TypeScript.
+        // Let TypeScript users catch these errors with the TypeScript compiler instead.
+        // Related: https://github.com/typescript-eslint/typescript-eslint/issues/342
+        'no-undef': 'off',
+      },
+    },
+  ],
   rules: {
     'no-var': 'error',
   },
